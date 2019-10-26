82d6d4fc5ea96f49437e473ef6ee5abdb771bf36
Alan Guzman <alanguzmanemail@gmail.com>
2019-10-26 19:03:01 -0400

    Frames now save in files corresponding to their video source.

--- autobound.py
+++ autobound.py
@@ -1,20 +1,33 @@
 import cv2
 import os
+from os import path
+
+def video_to_frames(video_name):
+
+    video_path = 'Input/' + video_name 
+    cap = cv2.VideoCapture(video_path)
+    output_path = 'Output/' + video_name;
+    
+    if not path.isdir(output_path):
+        os.makedirs(output_path)
+
+    count = 0
+
+    while cap.isOpened():
+        frameWasCaptured, frame = cap.read()
+        if frameWasCaptured:
+            cv2.imwrite(output_path + '/frame{:d}.jpg'.format(count), frame)
+            count += 30 # i.e. at 30 fps, this advances one second
+            cap.set(1, count)
+        else:
+            cap.release()
+            break
+
+input_path = 'Input'
+
+for file in os.listdir(input_path):
+    if file.endswith('.mp4'):
+        video_to_frames(file)
 
 
 
-
-cap = cv2.VideoCapture('Input/Store.mp4')
-os.makedirs('Output/Store.mp4')
-count = 0
-
-while cap.isOpened():
-    frameWasCaptured, frame = cap.read()
-
-    if frameWasCaptured:
-        cv2.imwrite('Output/Store.mp4/frame{:d}.jpg'.format(count), frame)
-        count += 30 # i.e. at 30 fps, this advances one second
-        cap.set(1, count)
-    else:
-        cap.release()
-        break
