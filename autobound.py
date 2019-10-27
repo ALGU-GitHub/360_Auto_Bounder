2cc17b77645185101a1929267cb45541eee40440
Alan Guzman <alanguzmanemail@gmail.com>
2019-10-27 09:19:17 -0400

    Altered frames and unaltered frames now save in seperate files.

--- autobound.py
+++ autobound.py
@@ -8,20 +8,34 @@ def video_to_frames(video_name):
 
     video_path = 'Input/' + video_name 
     video_capture = cv2.VideoCapture(video_path)
+    
     output_path = 'Output/' + video_name;
     
     if not path.isdir(output_path):
         os.makedirs(output_path)
+    
+    unaltered_output_path = output_path + '/Unaltered Frames'
+    
+    if not path.isdir(unaltered_output_path):
+        os.makedirs(unaltered_output_path)
+        
+    altered_output_path = output_path + '/Altered Frames'
+    
+    if not path.isdir(altered_output_path):
+        os.makedirs(altered_output_path)
+
 
     count = 0
-
+    angle_increment = 15
+    
     while video_capture.isOpened():
         frameWasCaptured, frame = video_capture.read()
         if frameWasCaptured:
-            for angle in np.arange(0, 30, 15):
+            cv2.imwrite(unaltered_output_path + '/*frame{:d}.jpg'.format(count), frame)
+            for angle in np.arange(angle_increment, 30, angle_increment):
                 rotated_frame = imutils.rotate_bound(frame, angle)     
-                cv2.imwrite(output_path + '/frame{:d}_{:d}.jpg'.format(count, angle), rotated_frame)
-            count += 200 # i.e. at 30 fps, this advances one second
+                cv2.imwrite(altered_output_path + '/frame{:d}_{:d}.jpg'.format(count, angle), rotated_frame)
+            count += 30 # i.e. at 30 fps, this advances one second
             video_capture.set(1, count)
         else:
             video_capture.release()
