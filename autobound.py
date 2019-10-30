999a10413efa81949cfb4f5e9504e95d9f191aa3
Alan Guzman <alanguzmanemail@gmail.com>
2019-10-30 10:57:46 -0400

    Rotation of frames moved to run_detection_on_image_at_path method.

--- autobound.py
+++ autobound.py
@@ -10,14 +10,21 @@ def make_directory_if_missing(directory_name):
         os.makedirs(directory_name)
 
 def run_detection_on_image_at_path(local_path_to_image):
-    autobound_path = os.getcwd()
-    true_path_to_image = os.getcwd() + '/' + local_path_to_image
-    darknet_path = 'darknet'
-    os.chdir(darknet_path)
-    os.system('./darknet detect cfg/yolov3.cfg cfg/yolov3.weights ' + true_path_to_image)
-    prediction_path = os.getcwd() + '/predictions.jpg'
-    shutil.copy(prediction_path, true_path_to_image + '_prediction')
-    os.chdir(autobound_path)
+    angle_increment = 15
+    image_from_path = cv2.imread(local_path_to_image)
+    
+    for angle in np.arange(15, 360, angle_increment):
+        rotated_frame = imutils.rotate_bound(image_from_path, angle)
+        new_image_path = local_path_to_image + '_{:d}.jpg'.format(angle)
+        cv2.imwrite(new_image_path, rotated_frame)
+        autobound_path = os.getcwd()
+        true_path_to_image = os.getcwd() + '/' + new_image_path
+        darknet_path = 'darknet'
+        os.chdir(darknet_path)
+        os.system('./darknet detect cfg/yolov3.cfg cfg/yolov3.weights ' + true_path_to_image)
+        prediction_path = os.getcwd() + '/predictions.jpg'
+        shutil.copy(prediction_path, true_path_to_image + '_prediction')
+        os.chdir(autobound_path)
 
 
 def video_to_frames(video_name):
@@ -38,20 +45,16 @@ def video_to_frames(video_name):
         frame_was_captured, frame = video_capture.read()
         if frame_was_captured:
             #cv2.imwrite(output_path + '/*frame{:d}.jpg'.format(count), frame)
-            for angle in np.arange(0, 15, angle_increment):
-                rotated_frame = imutils.rotate_bound(frame, angle)
-                new_image_path = output_path + '/frame{:d}_{:d}.jpg'.format(count, angle)
-                cv2.imwrite(new_image_path, rotated_frame)
-                run_detection_on_image_at_path(new_image_path)
-            count += 30 # i.e. at 30 fps, this advances one second
             video_capture.set(1, count)
         else:
             video_capture.release()
             break
 
 input_path = 'Input'
-
+run_detection_on_image_at_path('frame270_0.jpg')
+"""
 for file_in_input_path in os.listdir(input_path):
     if file_in_input_path.endswith('.mp4'):
         video_to_frames(file_in_input_path)
+"""
 print('Done')
