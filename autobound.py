0a702d78e24592f0ad9da84cc6fc095be0bbfd18
Alan Guzman <alanguzmanemail@gmail.com>
2019-11-11 16:02:27 -0500

    Output paths no longer include file extentions.

--- autobound.py
+++ autobound.py
@@ -11,30 +11,33 @@ def make_directory_if_missing(directory_name):
         os.makedirs(directory_name)
 
 def run_detection_on_image_at_path(local_path_to_image):
-    detections_path =  os.getcwd() + '/Detections'
-    make_directory_if_missing(detections_path)
-    
-    output_path = os.getcwd() + '/Output/' + local_path_to_image;
+    image_name = os.path.splitext(local_path_to_image)[0]
+    output_path = os.getcwd() + '/Output/' + image_name;
     make_directory_if_missing(output_path)
     
     angle_increment = 15
     image_from_path = cv2.imread(local_path_to_image)
     
+     
     for angle in np.arange(0, 360, angle_increment):
+        # Rotate current frame.
         rotated_frame = imutils.rotate(image_from_path, angle)
-        new_image_path = local_path_to_image + '_{:d}.jpg'.format(angle)
+        
+       
+        
+        new_image_path = output_path + '/' + image_name + '_{:d}.jpg'.format(angle)
         cv2.imwrite(new_image_path, rotated_frame)
+        
         autobound_path = os.getcwd()
-        true_path_to_image = os.getcwd() + '/' + new_image_path
         darknet_path = 'darknet'
         os.chdir(darknet_path)
-        os.system('./darknet detect cfg/yolov3.cfg cfg/yolov3.weights ' + true_path_to_image)
+        os.system('./darknet detect cfg/yolov3.cfg cfg/yolov3.weights ' + new_image_path)
         prediction_path = os.getcwd() + '/predictions.jpg'
-        shutil.copy(prediction_path, detections_path + '/' + new_image_path + '_prediction.jpg')
+        new_prediction_path = output_path + '/' + image_name + '_{:d}_prediction.jpg'.format(angle)
+        shutil.copy(prediction_path, new_image_path + '_prediction.jpg')
         os.remove(prediction_path)
         os.chdir(autobound_path)
-        shutil.copy(new_image_path, output_path)
-        os.remove(new_image_path)
+
 
 
 def draw_boarders_around_frame_at(frame_path):
@@ -71,10 +74,10 @@ def video_to_frames(video_name):
 
 
 input_path = 'Input'
-#run_detection_on_image_at_path('frame270_0.jpg')
+run_detection_on_image_at_path('Test.jpg')
 
-for file_in_input_path in os.listdir(input_path):
-    if file_in_input_path.endswith('.mp4'):
-        video_to_frames(file_in_input_path)
+#for file_in_input_path in os.listdir(input_path):
+#    if file_in_input_path.endswith('.mp4'):
+#        video_to_frames(file_in_input_path)
 
 print('Done')
